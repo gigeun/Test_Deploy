40299a41212c62c9827a9f9e1903c43ebbaf194e
admin <admin@DESKTOP-VFFPC66>
2023-06-19 17:07:08 +0900

    Main Class Update

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,20 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("Sample.fxml"));
+			Parent root = loader.load();
+			SampleController controller = loader.getController();
+			controller.setStage(primaryStage);
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,20 @@
 package application;
 
-public class SampleController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.stage.Stage;
+
+public class SampleController implements Initializable{
+
+	public void setStage(Stage primaryStage) {
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
 	
 }
